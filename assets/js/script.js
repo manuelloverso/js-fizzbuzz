55c967a06433d6b6f325534ef3fb4fd5c46420da
manuelloverso <manuelloverso@gmail.com>
2024-02-21 16:42:10 +0100

    add style to li

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,34 @@
 const ul = document.getElementById("list");
-let li = document.createElement("li");
 
 // Loop
 for (let i = 1; i <= 100; i++) {
+  let li = document.createElement("li");
   if (i % 3 == 0 && i % 5 == 0) {
-    console.log("fizzbuzz");
+    // console.log("fizzbuzz");
     // ul.innerHTML += `<li>FizzBuzz</li>`;
     ul.append(li);
     li.append("FizzBuzz");
-    console.log(li);
-    li.style.backgroundColor = "blue";
+    li.style.backgroundColor = "red";
     // li.append("FizzBuzz");
     // ul.append(li);
   } else if (i % 3 == 0) {
-    console.log("fizz");
-    ul.insertAdjacentHTML("beforeend", `<li>Fizz</li>`);
+    // console.log("fizz");
+    //ul.insertAdjacentHTML("beforeend", `<li>Fizz</li>`);
+
+    ul.append(li);
+    li.append("Fizz");
+    li.style.backgroundColor = "green";
   } else if (i % 5 == 0) {
-    console.log("buzz");
-    ul.insertAdjacentHTML("beforeend", `<li>Buzz</li>`);
+    // console.log("buzz");
+    // ul.insertAdjacentHTML("beforeend", `<li>Buzz</li>`);
+    ul.append(li);
+    li.append("Buzz");
+    li.style.backgroundColor = "yellowgreen";
   } else {
-    console.log(i);
-    ul.insertAdjacentHTML("beforeend", `<li>${i}</li>`);
+    // console.log(i);
+    //ul.insertAdjacentHTML("beforeend", `<li>${i}</li>`);
+    ul.append(li);
+    li.append(i);
+    li.style.backgroundColor = "steelblue";
   }
 }
