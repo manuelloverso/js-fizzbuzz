8b754b44f1257ecb87482165456eb3b07694d10f
manuelloverso <manuelloverso@gmail.com>
2024-02-21 17:11:13 +0100

    add background color by class

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ for (let i = 1; i <= 100; i++) {
     // console.log("fizzbuzz");
     // ul.innerHTML += `<li>FizzBuzz</li>`;
     li.innerText = "FizzBuzz";
-    li.style.backgroundColor = "red";
+    li.className = "bg-red";
   } else if (i % 3 == 0) {
     // console.log("fizz");
     //ul.insertAdjacentHTML("beforeend", `<li>Fizz</li>`);
