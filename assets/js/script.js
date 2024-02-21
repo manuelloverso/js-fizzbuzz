fd0e7f82559421e2c18f09f1d7e4b03e0ead77f0
manuelloverso <manuelloverso@gmail.com>
2024-02-21 16:30:56 +0100

    add grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,17 @@
 const ul = document.getElementById("list");
+let li = document.createElement("li");
 
 // Loop
 for (let i = 1; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
     console.log("fizzbuzz");
-    ul.insertAdjacentHTML("beforeend", `<li>FizzBuzz</li>`);
+    // ul.innerHTML += `<li>FizzBuzz</li>`;
+    ul.append(li);
+    li.append("FizzBuzz");
+    console.log(li);
+    li.style.backgroundColor = "blue";
+    // li.append("FizzBuzz");
+    // ul.append(li);
   } else if (i % 3 == 0) {
     console.log("fizz");
     ul.insertAdjacentHTML("beforeend", `<li>Fizz</li>`);
