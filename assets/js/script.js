ffde24ef3a676d76525059bcb11ce58051620768
manuelloverso <manuelloverso@gmail.com>
2024-02-21 15:49:03 +0100

    add  elements on dom

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,18 @@
+const ul = document.getElementById("list");
+
 // Loop
 for (let i = 1; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
     console.log("fizzbuzz");
+    ul.insertAdjacentHTML("beforeend", `<li>FizzBuzz</li>`);
   } else if (i % 3 == 0) {
     console.log("fizz");
+    ul.insertAdjacentHTML("beforeend", `<li>Fizz</li>`);
   } else if (i % 5 == 0) {
     console.log("buzz");
+    ul.insertAdjacentHTML("beforeend", `<li>Buzz</li>`);
   } else {
     console.log(i);
+    ul.insertAdjacentHTML("beforeend", `<li>${i}</li>`);
   }
 }
