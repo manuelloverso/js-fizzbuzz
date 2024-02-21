34636af429ebd5d7f3a6c52e88fa021a2044eef6
manuelloverso <manuelloverso@gmail.com>
2024-02-21 17:08:48 +0100

    fix innertext

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,29 +6,24 @@ for (let i = 1; i <= 100; i++) {
   if (i % 3 == 0 && i % 5 == 0) {
     // console.log("fizzbuzz");
     // ul.innerHTML += `<li>FizzBuzz</li>`;
-    ul.append(li);
-    li.append("FizzBuzz");
+    li.innerText = "FizzBuzz";
     li.style.backgroundColor = "red";
-    // li.append("FizzBuzz");
-    // ul.append(li);
   } else if (i % 3 == 0) {
     // console.log("fizz");
     //ul.insertAdjacentHTML("beforeend", `<li>Fizz</li>`);
 
-    ul.append(li);
-    li.append("Fizz");
+    li.innerText = "Fizz";
     li.style.backgroundColor = "green";
   } else if (i % 5 == 0) {
     // console.log("buzz");
     // ul.insertAdjacentHTML("beforeend", `<li>Buzz</li>`);
-    ul.append(li);
-    li.append("Buzz");
+    li.innerText = "Buzz";
     li.style.backgroundColor = "yellowgreen";
   } else {
     // console.log(i);
     //ul.insertAdjacentHTML("beforeend", `<li>${i}</li>`);
-    ul.append(li);
-    li.append(i);
+    li.innerText = i;
     li.style.backgroundColor = "steelblue";
   }
+  ul.append(li);
 }
